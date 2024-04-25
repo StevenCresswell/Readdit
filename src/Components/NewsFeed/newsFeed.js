d9c1e41c3793eae424d8be4856667c3fe6cad7df
Steven Cresswell <stevencresswell21@gmail.com>
2024-04-25 20:22:35 +0100

    fixed issues around improper selector syntax and added some code for individual post functionality

--- src/Components/NewsFeed/newsFeed.js
+++ src/Components/NewsFeed/newsFeed.js
@@ -1,21 +1,31 @@
 import React, {useEffect} from "react";
-import { loadNewsFeed, isLoadingData, failedLoadData } from "./newsFeedSlice";
+import { loadNewsFeed, isLoadingData, failedLoadData, selectSubreddit, changeSubreddit, selectData } from "./newsFeedSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 const NewsFeed = () => {
     const dispatch = useDispatch();
     const dataIsLoading = useSelector(isLoadingData);
     const dataLoadFailed = useSelector(failedLoadData);
-    const subReddit = "r/popular";
+    const posts = useSelector(selectData)
+    const subreddit = useSelector(selectSubreddit);
     useEffect(() => {
-        dispatch(loadNewsFeed(subReddit))
-    }, subReddit)
+        dispatch(loadNewsFeed(subreddit))
+    }, subreddit)
 
     if (dataIsLoading) return <div>Loading News Feed</div>;
     if (dataLoadFailed) return <div>Failed to Load News Feed</div>
 
+    const handleClick = (e) => {
+        dispatch(changeSubreddit(e.target.value))
+    }
+    /* put this in the return statement when you have made the post component:
+    {posts.map((post) => (
+        <Post post={post} />
+    ))}*/
     return (
-        <div>I worked!</div>
+        <div className="newsFeedContainer">
+            it worked!
+        </div>
     )
 }
 
--- src/Components/NewsFeed/newsFeedSlice.js
+++ src/Components/NewsFeed/newsFeedSlice.js
@@ -2,8 +2,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const loadNewsFeed = createAsyncThunk(
     'newsFeed/loadNewsFeed',
-    async (subReddit) => {
-        const response = await fetch(`https://www.reddit.com/${subReddit}.json`)
+    async (subreddit) => {
+        const response = await fetch(`https://www.reddit.com/${subreddit}.json`)
         const json = await response.json();
         return json.data.children;
     }
@@ -13,10 +13,15 @@ export const newsFeedSlice = createSlice({
     name: 'newsFeed',
     initialState: {
         newsFeedData: [],
+        currentSubreddit: 'r/popular',
         isLoadingData: false,
         failedLoadData: false,
     },
-    reducers: {},
+    reducers: {
+        changeSubreddit: (state, action) => {
+            state.currentSubreddit = action.payload
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(loadNewsFeed.pending, (state) => {
@@ -35,8 +40,9 @@ export const newsFeedSlice = createSlice({
     }
 });
 
-export const selectData = (state) => state.newsFeedData;
-export const isLoadingData = (state) => state.isLoadingData;
-export const failedLoadData = (state) => state.failedLoadData;
-
+export const selectData = (state) => state.newsFeed.newsFeedData;
+export const selectSubreddit = (state) => state.newsFeed.currentSubreddit;
+export const isLoadingData = (state) => state.newsFeed.isLoadingData;
+export const failedLoadData = (state) => state.newsFeed.failedLoadData;
+export const { changeSubreddit } = newsFeedSlice.actions;
 export default newsFeedSlice.reducer
